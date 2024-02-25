57707d529696b0a12d721627b02cff3d54361fc5
damaroja <ildefonsoarrecife3@hotmail.com>
2024-02-25 13:12:28 +0000

    Create Footer.jsx

---
+++ elementosHTML/Footer.jsx
@@ -0,0 +1,14 @@
+
+
+const Footer = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-1"> {/* Add any content here */}</main>
+      <footer className="text-center bg-gray-200 py-4">
+        APV - copyright 2024
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
