7e62edd06ea4de02f5be462a3c074201f7ab040a
damaroja <ildefonsoarrecife3@hotmail.com>
2024-02-25 17:19:17 +0000

    Update DosColumnas.jsx

--- Maquetacion/DosColumnas.jsx
+++ Maquetacion/DosColumnas.jsx
@@ -4,3 +4,11 @@
       <div><Formulario /></div>
       <div><ListadoPacientes /></div>
     </div>
+
+//Otra forma es con Flex:
+
+<div className="flex">
+      <div className="md:w-1/2"><Formulario /></div>
+      <div className="md:w-1/2"><ListadoPacientes /></div>
+    </div>
+
